e809f95a0717eec6be6964d6e8fb97a102664e27
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-08-24 13:56:12 +0530

    add meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Herbert",
   description: "Herbert",
+  // metadataBase: new URL(""),
+  openGraph: {
+    images: "/home-meta-img.png",
+  },
 };
 
 export default function RootLayout({ children }) {
